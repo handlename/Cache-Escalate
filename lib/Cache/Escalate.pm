15c92a9a1b9de01602a33b8c8909128c2d7dd93d
handlename <nagata@handlena.me>
2016-03-03 17:34:17 +0900

    implement minimum functions

--- lib/Cache/Escalate.pm
+++ lib/Cache/Escalate.pm
@@ -3,9 +3,98 @@ use 5.008001;
 use strict;
 use warnings;
 
+use Class::Accessor::Lite (
+    ro => [qw{caches sync_level sync_expiration}],
+);
+
 our $VERSION = "0.01";
 
+our $SYNC_LEVEL_NONE   = 1;
+our $SYNC_LEVEL_MISSED = 2;
+our $SYNC_LEVEL_FULL   = 3;
 
+sub new {
+    my $class = shift;
+    my %args = @_;
+
+    if (! $args{caches} || ref $args{caches} ne "ARRAY" || ! scalar @{$args{caches}}) {
+        die "One more caches required.";
+    }
+
+    for my $cache (@{$args{caches}}) {
+        if (! _looks_like_cache($cache)) {
+            die "`caches` contains invalid object.";
+        }
+    }
+
+    return bless {
+        caches     => $args{caches},
+        sync_level => $args{sync_level},
+    }
+}
+
+sub _looks_like_cache {
+    my ($cache) = @_;
+
+    for my $method (qw{get set delete}) {
+        return if ! $cache->can($method);
+    }
+
+    return 1;
+}
+
+sub get {
+    my ($self, $key) = @_;
+
+    my $value;
+    my @caches = @{$self->caches};
+    my @missed;
+    my @rest;
+
+    while (my $cache = shift @caches) {
+        $value = $cache->get($key);
+
+        if (defined $value) {
+            @rest = @caches;
+            last;
+        }
+
+        push @missed, $cache;
+    }
+
+    return if ! defined $value;
+    return $value if $self->sync_level == $SYNC_LEVEL_NONE;
+
+    my @sync_targets = $self->sync_level == $SYNC_LEVEL_MISSED
+        ? @missed
+        : (@missed, @rest);
+
+    for my $cache (@sync_targets) {
+        $cache->set($key, $value, $self->sync_expiration);
+    }
+
+    return $value;
+}
+
+sub set {
+    my ($self, $key, $value, $expiration) = @_;
+
+    for my $cache (@{$self->caches}) {
+        $cache->set($key, $value, $expiration);
+    }
+
+    return;
+}
+
+sub delete {
+    my ($self, $key) = @_;
+
+    for my $cache (@{$self->caches}) {
+        $cache->delete($key);
+    }
+
+    return;
+}
 
 1;
 __END__
