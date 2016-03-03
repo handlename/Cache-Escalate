7a03de00c381d04456f4818d442b687dbc8dc2c2
handlename <nagata@handlena.me>
2016-03-03 15:42:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Cache::Escalate - It's new $module
+
+# SYNOPSIS
+
+    use Cache::Escalate;
+
+# DESCRIPTION
+
+Cache::Escalate is ...
+
+# LICENSE
+
+Copyright (C) handlename.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+handlename &lt;handle@cpan.org>
---
+++ lib/Cache/Escalate.pm
@@ -0,0 +1,38 @@
+package Cache::Escalate;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Cache::Escalate - It's new $module
+
+=head1 SYNOPSIS
+
+    use Cache::Escalate;
+
+=head1 DESCRIPTION
+
+Cache::Escalate is ...
+
+=head1 LICENSE
+
+Copyright (C) handlename.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+handlename E<lt>handle@cpan.orgE<gt>
+
+=cut
